818de2b15a4251b47b4f5bd4f52057849fd4f36d
Cooper <Cooperransom08@gmail.com>
2024-01-20 23:49:30 -0500

    finish vpn_script 1.0

--- source/vpn_script.py
+++ source/vpn_script.py
@@ -1,6 +1,21 @@
 import os, sys
 import requests
 import time
+import socket
+
+
+class Globals:
+  host_name = socket.gethostname()
+  ip = socket.gethostbyname(host_name)
+  def getIP():
+    try:
+      response = requests.get('https://api.ipify.org?format=json')
+      data = response.json()
+      public_ip = data.get('ip')
+      return public_ip
+    except requests.RequestException as e:
+      print(f"Error: {e}")
+      return None
 
 
 def is_online() -> bool:
@@ -31,9 +46,9 @@ def parallel_commands(commands: list) -> str:
   '''
   separator = '&&' if os.name != 'nt' else ';'
   full_command = separator.join(commands)
-  print(f'powershell {full_command}')
   result = os.popen(f'powershell {full_command}').read()
-  print(result)
+  if result != '':
+    print(result)
   return result
 
 
@@ -52,15 +67,8 @@ def disconnect_and_reconnect(trouble_shoot: bool = False) -> None:
   '''
   Disconnects then reconnects VPN
   '''
-  if trouble_shoot:
-    time.sleep(2)
-    if not is_online():
-      disconnect_vpn()
-      time.sleep(1)
-      connect_vpn()
-  else:
-    disconnect_vpn()
-    connect_vpn()
+  disconnect_vpn()
+  connect_vpn()
 
 
 def connect_vpn(connect_opt: str = 'default') -> None:
@@ -84,35 +92,35 @@ def connect_vpn(connect_opt: str = 'default') -> None:
     if connect_opt == 'default':
       commands_list = [cd_cmd, f'./{exe_name} -c']
       parallel_commands(commands_list)
-      disconnect_and_reconnect(True)
+      print('Connected to the closest server...')
 
     elif connect_opt == 'dedicated' or connect_opt == 'personal':
-      commands_list = [cd_cmd, f'./{exe_name} -c -g {connect_options[0]}']
-      parallel_commands(commands_list)
-      disconnect_and_reconnect(True)
-
-    elif connect_opt == 'dual' or connect_opt == 'double':
       commands_list = [cd_cmd, f'./{exe_name} -c -g {connect_options[1]}']
       parallel_commands(commands_list)
-      disconnect_and_reconnect(True)
+      print(f'Connected to the most optimal "{connect_options[1]}" server...')
 
-    elif connect_opt == 'secure' or connect_opt == 'obfuscated':
+    elif connect_opt == 'dual' or connect_opt == 'double':
       commands_list = [cd_cmd, f'./{exe_name} -c -g {connect_options[2]}']
       parallel_commands(commands_list)
-      disconnect_and_reconnect(True)
+      print(f'Connected to the closest "{connect_options[2]}" server...')
+
+    elif connect_opt == 'secure' or connect_opt == 'obfuscated':
+      commands_list = [cd_cmd, f'./{exe_name} -c -g {connect_options[3]}']
+      parallel_commands(commands_list)
+      print(f'Connected to the closest "{connect_options[3]}" server...')
 
     elif connect_opt == 'onion' or connect_opt == 'tor':
       commands_list = [cd_cmd, f'./{exe_name} -c -g {connect_options[4]}']
       parallel_commands(commands_list)
-      disconnect_and_reconnect(True)
+      print(f'Connected to the closest "{connect_options[4]}" server...')
 
     elif connect_opt == 'p2p' or connect_opt == 'peer':
       commands_list = [cd_cmd, f'./{exe_name} -c -g {connect_options[5]}']
       parallel_commands(commands_list)
-      disconnect_and_reconnect(True)
+      print(f'Connected to the closest "{connect_options[5]}" server...')
 
 
-if __name__ == '__main__':
+def arg_handler() -> None:
   try:
     if __file__.endswith('.py'):
       arg1 = sys.argv[1].lower()
@@ -123,6 +131,35 @@ if __name__ == '__main__':
   except Exception:
     pass
 
-  if arg1 == 'disconnect':
-    disconnect_vpn()
-  connect_vpn(arg1)
+  connected_to_vpn = 'VPN OFF'
+
+  match arg1:
+
+    case '-c':
+      connected_to_vpn = 'VPN ON'
+      if 'arg2' not in locals():
+        arg2 = 'default'
+      connect_vpn(arg2)
+    
+    case '-d':
+      connected_to_vpn = 'VPN OFF'
+      print(f'Disconnecting from VPN...')
+      disconnect_vpn()
+    
+    case '-dr' | '-d&r':
+      disconnect_and_reconnect()
+      print(f'VPN has been reset...')
+    
+    case '-s':
+      status = 'ONLINE' if is_online() else 'OFFLINE'
+      print(
+        f'********************************** \
+        \n >  {socket.gethostname()} is {status} \
+        \n >  Assumed Status: {connected_to_vpn} \
+        \n >  IP Address: {Globals.getIP()} \
+        \n**********************************'
+      )
+
+
+if __name__ == '__main__':
+  arg_handler()
